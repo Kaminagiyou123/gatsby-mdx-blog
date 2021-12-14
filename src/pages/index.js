adabab1e83225dafe6dab6623f0366a67b77d709
Ran You <ran.you8311@gmail.com>
2021-12-14 12:56:26 -0800

    post complete

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,41 @@ import Hero from '../components/Hero'
 import Layout from '../components/Layout'
 import Posts from '../components/Posts'
 import { graphql } from 'gatsby'
-const IndexPage = () => {
+const IndexPage = ({ data }) => {
+  const {
+    allMdx: { nodes: posts },
+  } = data
   return (
     <Layout>
       <Hero showPerson />
+      <Posts posts={posts} title="recently published" />
     </Layout>
   )
 }
+
+export const query = graphql`
+  {
+    allMdx(limit: 3, sort: { fields: frontmatter___date, order: DESC }) {
+      nodes {
+        id
+        excerpt
+        frontmatter {
+          title
+          author
+          category
+          date(formatString: "MMMM, Do YYYY")
+          readTime
+          slug
+          image {
+            childImageSharp {
+              gatsbyImageData
+            }
+          }
+        }
+        body
+      }
+    }
+  }
+`
 
 export default IndexPage
--- src/pages/posts.js
+++ src/pages/posts.js
@@ -3,10 +3,41 @@ import Layout from '../components/Layout'
 import Hero from '../components/Hero'
 import { graphql } from 'gatsby'
 import Posts from '../components/Posts'
-const PostsPage = () => {
-return <h4>posts page</h4>
+const PostsPage = ({ data }) => {
+  const {
+    allMdx: { nodes: posts },
+  } = data
+  return (
+    <Layout>
+      <Hero />
+      <Posts posts={posts} title="All Posts" />
+    </Layout>
+  )
 }
 
-
+export const query = graphql`
+  {
+    allMdx(sort: { fields: frontmatter___date, order: DESC }) {
+      nodes {
+        id
+        frontmatter {
+          title
+          author
+          category
+          date(formatString: "MMMM, Do YYYY")
+          readTime
+          slug
+          image {
+            childImageSharp {
+              gatsbyImageData
+            }
+          }
+        }
+        body
+        excerpt
+      }
+    }
+  }
+`
 
 export default PostsPage
