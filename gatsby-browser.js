1059270dd29070dd93988c087582a7be845d795d
Ran You <ran.you8311@gmail.com>
2021-12-17 13:53:29 -0800

    mdx wrapper complete

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,7 @@
 import './src/css/main.css'
+import React from 'react'
+import { MDXProvider } from '@mdx-js/react'
+
+import { wrapMDX } from './root-mdx'
+
+export const wrapRootElement = wrapMDX
---
+++ gatsby-ssr.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import { MDXProvider } from '@mdx-js/react'
+
+import { wrapMDX } from './root-mdx'
+
+export const wrapRootElement = wrapMDX
